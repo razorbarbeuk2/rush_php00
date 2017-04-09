6f165011662d7a3aa2db185e48bef852ee459a2f
gbourson <gbourson@student42.fr>
2017-04-09 14:26:12 +0200

    MAJ front

--- index.php
+++ index.php
@@ -7,34 +7,38 @@ if ($_GET['name'] != NULL && $_GET['quantite'] != NULL && $_GET['submit'] == OK)
 ?>
 <html>
 <head>
-<title>Rent Club</title>
-<h1><img id="title" src="font.png"></h1>
-<link rel="stylesheet" href="style.css">
+	<title>Rent Club</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="./style.css">
 </head>
-<div class="header">
-	<a href="#"><img id="cart" src="http://le-ecommerce.com/wp-content/uploads/2014/10/la-formule-du-panier-moyen-1.jpg"></a>
-	<form action="panier.php" method="POST"><button id='validation'><input type='submit' name='submit' value='Valider la commande'></button></form>
-	<div class="info">
-	</div>
-</div>
 <body>
-<?php
-$conn = connexion();
-$req = mysqli_query($conn, "SELECT id_objet, name, value, url FROM objet;");
-$tab = array();
-while ($arr = mysqli_fetch_array($req)){
-	$arr[id_objet];
-	$arr[name];
-	$arr[value];
-	$arr[url];
-echo"<div class='img'>";
-echo"<form action='' method='GET'>";
-echo"<img src='$arr[url]' name='$arr[name]'><br />";
-echo"<input id='input' type='text' name='quantite' value=''>";
-echo"<input id='input' type='hidden' name='name' value='$arr[name]'>";
-echo"<input id='input' type='submit' name='submit' value='OK'>";
-echo"</form></div>";
-}
-?>
-</body>
-</html>
+	<div class="wrapper">
+		<header>
+			<h1><img src="./img/font.png" alt="title"></h1>
+			<div class="content_cart">
+				<div class="cart"></div>
+				<form action="panier.php" method="POST">
+					<input class="btn" type='submit' name='submit' value='Valider la commande'>
+				</form>
+			</div>
+		</header>
+		<?php
+		$conn = connexion();
+		$req = mysqli_query($conn, "SELECT id_objet, name, value, url FROM objet;");
+		$tab = array();
+		while ($arr = mysqli_fetch_array($req)){
+			$tab[] = $arr;
+			?>
+			<div class="img">
+				<form action="" method="GET">
+					<img src="<?php echo $arr['url']; ?>" name="<?php echo $arr['name']; ?>"><br />
+					<input class="index_input quantity" type="text" name="quantite" value="">
+					<input class="index_input" type="hidden" name="name" value="<?php echo $arr['name']; ?>">
+					<p><?php echo $arr['value']; ?> Euros/h</p>
+					<input class="index_input btn" type="submit" name="submit" value="OK">
+				</form>
+			</div>
+			<?php } ?>
+		</div>
+	</body>
+	</html>
